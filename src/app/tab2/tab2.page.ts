22446690d809566eea4e4ca459790a9d44817163
PierGiulioMinetti <pg.minetti@gmail.com>
2024-01-18 17:56:44 +0100

    added camera icon

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonCol, IonRow, IonImg } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonFab, IonFabButton, IonIcon, IonGrid, IonCol, IonRow, IonImg, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
-import { PhotoService } from '../services/photo.service';
+import { PhotoService, UserPhoto } from '../services/photo.service';
 import { NgFor, NgIf } from '@angular/common';
+import { ActionSheetController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -10,6 +11,10 @@ import { NgFor, NgIf } from '@angular/common';
   styleUrls: ['tab2.page.scss'],
   standalone: true,
   imports: [
+    //angular
+    NgFor,
+    NgIf,
+    //ionic
     IonHeader,
     IonToolbar,
     IonTitle,
@@ -22,17 +27,52 @@ import { NgFor, NgIf } from '@angular/common';
     IonCol,
     IonRow,
     IonImg,
-    NgFor,
-    NgIf
+    IonButton
+
   ]
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+
+  async ngOnInit() {
+    await this.photoService.loadSaved();
+  }
 
 
-  constructor(public photoService: PhotoService) { }
+  constructor(
+    public photoService: PhotoService,
+    public actionSheetController: ActionSheetController
+    ) { }
 
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
+
+  deletePhotos(){
+    this.photoService.photos = [];
+  }
+
+  public async showActionSheet(photo: UserPhoto, position: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Photos',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          this.photoService.deletePicture(photo, position);
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          // Nothing to do, action sheet is automatically closed
+          }
+      }]
+    });
+    await actionSheet.present();
+  }
+
+
 
 }
